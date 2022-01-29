3c6bd5ba8a80023a7715e761f95a369609f9debc
shilalyelken <shilalyelken@gmail.com>
2022-01-29 22:57:06 +0300

    commit hilal

--- src/main/java/common/team3.java
+++ src/main/java/common/team3.java
@@ -2,26 +2,4 @@ package common;
 
 public class team3 {
 
-    public static void main(String[] args) {
-
-        prime(29);
-
-    }
-
-    public static void prime(int num) {
-        boolean flag = false;
-        for (int i = 2; i <= num / 2; ++i) {
-            if (num % i == 0) {
-                flag = true;
-                break;
-            }
-        }
-
-        if (!flag)
-            System.out.println(num + " is a prime number.");
-        else
-            System.out.println(num + " is not a prime number.");
-
-    }
 }
-
