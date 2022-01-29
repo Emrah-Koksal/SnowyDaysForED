e8900fcdfdc5dcac018d8aedb1cdbc5777c4f677
shilalyelken <shilalyelken@gmail.com>
2022-01-29 23:05:50 +0300

    commit hilal

--- src/main/java/common/team3.java
+++ src/main/java/common/team3.java
@@ -1,5 +1,13 @@
 package common;
 
 public class team3 {
-
+    //Write a Java Program to find whether a number is prime or not.
+    public static void main(String[] args) {
+        int num = 0;
+        if (num%2!=0 || num>=2) {
+            System.out.println("This is a prime number");
+        }else {
+            System.out.println("This is not a prime number");
+        }
+    }
 }
