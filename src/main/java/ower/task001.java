4f489285be8ee86cc14e7a4825cc01bd9d3b562a
owermaiden <ower.maiden@cybertekschool.com>
2022-01-29 21:57:45 +0300

    ower

---
+++ src/main/java/ower/task001.java
@@ -0,0 +1,13 @@
+package ower;
+
+public class task001 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println(a + "|" + b);
+    }
+}
