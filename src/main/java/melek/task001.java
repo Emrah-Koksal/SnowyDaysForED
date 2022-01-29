3156d43b1d7c24e5c687c60ea60422342b04ad01
Melek <hmlkshn@gmail.com>
2022-01-29 21:58:08 +0300

    melekTask

---
+++ src/main/java/melek/task001.java
@@ -0,0 +1,19 @@
+package melek;
+
+public class task001 {
+    public static void main(String[] args) {
+        //Write a Java Program to swap two numbers using the third variable.
+        int num1=7;
+        int num2=10;
+        int temp;
+
+        temp=num1;
+        num1=num2;
+        num2=temp;
+        System.out.println("x: "+num1+"  y: "+num2);
+
+
+
+
+    }
+}
