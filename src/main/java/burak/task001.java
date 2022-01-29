f4b18999dfb2e5cd5726bcf89d0681c213d711dc
Abdullah Burak Aksoy <aburakaksoy@hotmail.com>
2022-01-29 21:58:22 +0300

    brk first commit

---
+++ src/main/java/burak/task001.java
@@ -0,0 +1,18 @@
+package burak;
+
+public class task001 {
+
+
+    public static void main(String[] args) {
+    //Write a Java Program to swap two numbers using the third variable.
+
+    int a=5,b=3,c=a+b;
+
+    b=a;
+    a=c-b;
+
+        System.out.println(a+" , "+b);
+
+
+    }
+}
